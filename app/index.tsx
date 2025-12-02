e6bf50d2370254edd756909dab2e952f72547791
alenamis2189 <alenamis2189@gmail.com>
2025-12-01 21:26:01 -0600

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -8,7 +8,7 @@ export default function HomeScreen() {
     router.push('/difficulty');
   }
   function seeHighScores(){
-    router.push('/highScores');
+    router.push('/highScore');
   }
 
   return (
