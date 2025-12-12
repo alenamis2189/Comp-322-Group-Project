56261f75da10445aac3f9769cf979a40fb180745
Francisco Garzon <68824219+fagc@users.noreply.github.com>
2025-12-11 18:28:30 -0600

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,14 @@
 import { router } from 'expo-router';
+import React from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
 
-  // Start game
-  function startTSAGame(){
+  function startTSAGame() {
     router.push('/difficulty');
   }
-  function seeHighScores(){
+
+  function seeHighScores() {
     router.push('/highScores');
   }
 
@@ -16,7 +17,7 @@ export default function HomeScreen() {
       <Text style={styles.icon}>✈️</Text>
       <Text style={styles.title}>TSA Bag Ready</Text>
       <Text style={styles.subtitle}>Test your security skills!</Text>
-      
+
       {/* Start Button with press feedback */}
       <Pressable 
         onPress={startTSAGame}
@@ -24,15 +25,14 @@ export default function HomeScreen() {
       >
         <Text style={styles.buttonText}>Start Game</Text>
       </Pressable>
+
       {/* go to high scores screen */}
       <Pressable 
         onPress={seeHighScores}
-        style={({ pressed }) => [styles.button, pressed && styles.buttonPressed]} 
+        style={({ pressed }) => [styles.buttonSecondary, pressed && styles.buttonPressed]} 
       >
         <Text style={styles.buttonText}>High Scores</Text>
       </Pressable>
-
-
     </View>
   );
 };
@@ -60,18 +60,32 @@ const styles = StyleSheet.create({
   subtitle: {
     fontSize: 17,
     color: '#777777',
-    marginBottom: 55,
+    marginBottom: 30, // a bit less because we added input -fg
     textAlign: 'center',
     letterSpacing: 0.6,
   },
   button: {
-    backgroundColor: '#007AFF', // primary blue
+    backgroundColor: '#28A745', // primary green
     paddingVertical: 16,
     paddingHorizontal: 30,
     borderRadius: 12,
     minWidth: 220,
     alignItems: 'center',
     marginBottom: 14, // space between buttons
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.12,
+    shadowRadius: 5,
+    elevation: 3,
+  },
+  buttonSecondary: { //-fg to make it different color
+    backgroundColor: '#007AFF',
+    paddingVertical: 16,
+    paddingHorizontal: 30,
+    borderRadius: 12,
+    minWidth: 220,
+    alignItems: 'center',
+    marginBottom: 14,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.12,
